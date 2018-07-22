b97dcb1ca33d7495382d6c7ef47383466e5d4bf1
philtd5000 <philiptaft9@gmail.com>
2018-07-22 15:21:22 -0700

    initial site files and Letter constructor

---
+++ letter.js
@@ -0,0 +1,24 @@
+module.exports = Letter;
+
+function Letter(char, boolean) {
+    this.char = char.toLowerCase();
+    this.boolean = boolean;
+
+    this.returnChar = () => {
+        if (this.boolean === true) {
+            console.log(this.char);
+            return this.char;
+        } else {
+            console.log('_');
+            return "_";
+        }
+    }
+
+    this.checkChar = (guessedChar) => {
+        (guessedChar.toLowerCase() === this.char) ? this.boolean = true : this.boolean = false;
+        console.log(this.boolean);
+    }
+
+
+}
+
---
+++ word.js
@@ -0,0 +1,9 @@
+let Letter = require('./letter');
+
+let a = new Letter('a', false);
+
+//CHECKED AND WORKING:
+a.checkChar("a");
+a.returnChar();
+a.checkChar("z");
+a.returnChar();
